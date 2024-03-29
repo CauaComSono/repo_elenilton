aa3fada624f0509ab0c234b18286d751ea79e227
unknown <cekschadeck@minha.fag.edu.br>
2024-03-28 21:22:27 -0300

    classes

---
+++ Empresa.java
@@ -0,0 +1,19 @@
+public class Empresa {
+
+    int CNPJ;
+    String nome;
+    
+    public int getCNPJ() {
+        return CNPJ;
+    }
+    public void setCNPJ(int cNPJ) {
+        CNPJ = cNPJ;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+}
---
+++ src/Funcionario.java
@@ -0,0 +1,43 @@
+public class Funcionario {
+
+    int cpf;
+    String nome;
+    int telefone;
+    String funcao;
+
+    public int cpf(){
+        return cpf;
+    }
+
+    public String nome(){
+        return nome;
+    }
+
+    public int telefone(){
+        return telefone;
+    }
+
+    public String funcao(){
+        return funcao;
+    }
+
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+
+    public void setFuncao(String funcao) {
+        this.funcao = funcao;
+    }
+}
+    
+    
+
